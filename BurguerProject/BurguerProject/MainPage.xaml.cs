aa3dae2d57207838bdc7692e91cc69cd0cbe4ed5
mrlramos <marlon_raoni10@hotmail.com>
2021-04-18 02:29:45 -0300

    First commit and creation of models

---
+++ BurguerProject/BurguerProject/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BurguerProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalvar(object sender, EventArgs e)
+        {
+            Preferences.Set("username", CoSeUs.Text);
+            var teste = Preferences.Get("username", "default_value");
+            
+            if (teste == "Marlon")
+            {
+                var teste2 = "asdas";
+            }
+        }
+
+        private void BtnRetrieveClicked(object sender, EventArgs e)
+        {
+            var retorno = Preferences.Get("username", "");
+            LabRet.Text = retorno;
+        }
+    }
+}
